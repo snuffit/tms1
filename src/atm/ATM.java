fa22163c7c8e45ce6cea67370226ea94ae5767da
Stas <sadpufikot@gmail.com>
2026-03-18 21:55:15 +0300

    make hw6

---
+++ src/atm/ATM.java
@@ -0,0 +1,73 @@
+package atm;
+
+/**
+ * @author Pstyga Stas
+ * @created 18.03.2026
+ */
+public class ATM {
+    //Создать класс, описывающий банкомат. Набор купюр, находящихся в банкомате, должен
+    //задаваться тремя свойствами: количеством купюр номиналом 20, 50 и 100. Сделать
+    //метод для добавления денег в банкомат. Сделать функцию, снимающую деньги, которая
+    //принимает сумму денег, а возвращает булевое значение - успешность выполнения
+    //операции. При снятии денег, функция должна распечатывать каким количеством купюр
+    //какого номинала выдаётся сумма. Создать конструктор с тремя параметрами -
+    //количеством купюр каждого номинала.
+
+    private int cashValueTwenty;
+    private int cashValueFifty;
+    private int cashValueHungered;
+
+    public ATM(int cashValueTwenty, int cashValueFifty, int cashValueHungered) {
+        this.cashValueTwenty = cashValueTwenty;
+        this.cashValueFifty = cashValueFifty;
+        this.cashValueHungered = cashValueHungered;
+    }
+
+    public void addCash(int cashValueTwenty, int cashValueFifty, int cashValueHungered) {
+        this.cashValueTwenty += cashValueTwenty;
+        this.cashValueFifty += cashValueFifty;
+        this.cashValueHungered += cashValueHungered;
+    }
+
+    public boolean withdrawCash(int cash) {
+        //Как по мне, выглядит не очень, но это та логика, которую я придумал сам
+        int remains = cash;
+        int cashValueTwenty = 0;
+        int cashValueFifty = 0;
+        int cashValueHungered = 0;
+        if (remains / 100 > 0) {
+            cashValueHungered = remains / 100;
+            remains -= cashValueHungered * 100;
+        }
+        if (remains > 60 || remains == 50) {
+            cashValueFifty = 1;
+            remains -= 50;
+        }
+        if (remains / 20 > 0) {
+            cashValueTwenty = remains / 20;
+            remains -= cashValueTwenty * 20;
+        }
+        if (remains == 0 &&
+        this.cashValueTwenty > cashValueTwenty &&
+        this.cashValueFifty > cashValueFifty &&
+        this.cashValueHungered > cashValueHungered) {
+            this.cashValueTwenty -= cashValueTwenty;
+            this.cashValueFifty -= cashValueFifty;
+            this.cashValueHungered -= cashValueHungered;
+            System.out.println("Запрошено " + cash);
+            System.out.printf("Выдано 100 = %d, 50 = %d, 20 = %d\n",
+                    cashValueHungered, cashValueFifty, cashValueTwenty);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ATM: " +
+                "cashValueTwenty=" + cashValueTwenty +
+                ", cashValueFifty=" + cashValueFifty +
+                ", cashValueHungered=" + cashValueHungered;
+    }
+}
