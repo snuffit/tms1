9fa35b3b944f0a77973fc28c53ec88f2ce293a5f
Simon Pirko <sipi7gm@gmail.com>
2026-05-26 16:43:13 +0300

    Revert "Выполнено ДЗ 23"

--- src/main/java/xml_parsers/XMLParser.java
+++
@@ -1,9 +0,0 @@
-package xml_parsers;
-
-import java.util.List;
-
-public interface XMLParser {
-
-    String getValueByTagName(String tagName);
-    List<String> getListValuesByTagName(String tagName);
-}
